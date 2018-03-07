b89c6d61b655ac5aaad322fd2a495ebafcfb89dd
ben <benjenkinsv95@gmail.com>
2018-03-06 21:49:07 -0500

    Adding dev-propose command for formatted message.

--- index.js
+++ index.js
@@ -138,28 +138,53 @@ controller.on('slash_command', function (slashCommand, message) {
                 proposal = "I propose " + proposal;
             }
 
-            slashCommand.reply(message, {
-                attachments:[
-                    {
-                        title: 'Do you want to interact with my buttons?',
-                        callback_id: '123',
-                        attachment_type: 'default',
-                        actions: [
-                            {
-                                "name":"yes",
-                                "text": "Yes",
-                                "value": "yes",
-                                "type": "button",
-                            },
-                            {
-                                "name":"no",
-                                "text": "No",
-                                "value": "no",
-                                "type": "button",
-                            }
-                        ]
-                    }
-                ]
+            slashCommand.startConversation(message, function(err, convo) {
+
+                convo.ask({
+                              attachments:[
+                                  {
+                                      title: 'Do you want to proceed?',
+                                      callback_id: '123',
+                                      attachment_type: 'default',
+                                      actions: [
+                                          {
+                                              "name":"yes",
+                                              "text": "Yes",
+                                              "value": "yes",
+                                              "type": "button",
+                                          },
+                                          {
+                                              "name":"no",
+                                              "text": "No",
+                                              "value": "no",
+                                              "type": "button",
+                                          }
+                                      ]
+                                  }
+                              ]
+                          },[
+                              {
+                                  pattern: "yes",
+                                  callback: function(reply, convo) {
+                                      convo.say('FABULOUS!');
+                                      convo.next();
+                                      // do something awesome here.
+                                  }
+                              },
+                              {
+                                  pattern: "no",
+                                  callback: function(reply, convo) {
+                                      convo.say('Too bad');
+                                      convo.next();
+                                  }
+                              },
+                              {
+                                  default: true,
+                                  callback: function(reply, convo) {
+                                      // do nothing
+                                  }
+                              }
+                          ]);
             });
 
             break;
@@ -170,29 +195,3 @@ controller.on('slash_command', function (slashCommand, message) {
 })
 ;
 
-controller.hears('interactive', 'direct_message', function(bot, message) {
-
-    bot.reply(message, {
-        attachments:[
-            {
-                title: 'Do you want to interact with my buttons?',
-                callback_id: '123',
-                attachment_type: 'default',
-                actions: [
-                    {
-                        "name":"yes",
-                        "text": "Yes",
-                        "value": "yes",
-                        "type": "button",
-                    },
-                    {
-                        "name":"no",
-                        "text": "No",
-                        "value": "no",
-                        "type": "button",
-                    }
-                ]
-            }
-        ]
-    });
-});
